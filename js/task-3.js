5b5909fc2de49b1423453600e530689361da1e00
AnnaRoska <murkesha2014@gmail.com>
2025-07-22 21:38:50 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ const showName = document.querySelector('#name-output')
 userNameElem.addEventListener('input', () => {
     const value = userNameElem.value;
 
-    if (value.length>=1) {
+    if (value.trim().length>=1) {
         showName.textContent = value.trim();
     } else {
         showName.textContent = "Anonymous";
